17aab9323e26c82bf172729f06477646173592b5
Juan Mercurio <juanmercuriobadii@gmail.com>
2022-07-25 22:27:55 -0300

    Let make it gay

--- ballClicker.js
+++ ballClicker.js
@@ -2,21 +2,16 @@ var Circle = /** @class */ (function () {
     function Circle(context) {
         this.context = context;
         this.r = ballSize;
-        this.x = Math.min(Math.random() * (canvas.width) + this.r, canvas.width - this.r);
-        this.y = Math.min(Math.random() * (canvas.height) + this.r, canvas.height - this.r);
-        this.color = "#2FCBE7";
-        this.direccion = [Math.random() * (1 - (-1)) + (-1), Math.random() * (1 - (-1)) + (-1)];
+        this.x = Math.min(randomEntre(canvas.width, this.r), canvas.width - this.r);
+        this.y = Math.min(randomEntre(canvas.height, this.r), canvas.height - this.r);
+        this.color = new Color(0, 0, 0, 0.8); //new Color(47, 203, 231, 0.8)
+        this.direccion = [randomEntre(-1, 1), randomEntre(-1, 1)];
         this.velocidad = velocidad;
     }
     Circle.prototype.draw = function () {
         this.x += this.direccion[0] * velocidad;
         this.y += this.direccion[1] * velocidad;
-        // esto es el borde
-        // this.context.fillStyle = "#181818";
-        // this.context.beginPath();
-        // this.context.arc(this.x, this.y, this.r+2, 0, 2 * Math.PI);
-        // this.context.fill();
-        this.context.fillStyle = this.color;
+        this.context.fillStyle = this.color.toString();
         this.context.beginPath();
         this.context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         this.context.fill();
@@ -28,10 +23,64 @@ function clearScreen() {
     context.fillStyle = "#181818";
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
+var Color = /** @class */ (function () {
+    function Color(r, g, b, o) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.o = o;
+    }
+    Color.prototype.dropOpacity = function (n) {
+        this.o -= n;
+    };
+    Color.prototype.toString = function () {
+        return "rgb(".concat(this.r.toString(), ",", this.g.toString(), ",", this.b.toString(), ",", this.o.toString(), ")");
+    };
+    Color.prototype.getOpacity = function () {
+        return this.o;
+    };
+    Color.prototype.random = function () {
+        this.r = randomEntre(0, 255);
+        this.g = randomEntre(0, 255);
+        this.b = randomEntre(0, 255);
+    };
+    return Color;
+}());
+function randomEntre(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function particlesCreate(x, y, color) {
+    for (var i = 0; i < 10; i++) {
+        var particle = new Circle(context);
+        particle.x = x;
+        particle.y = y;
+        particle.color.random();
+        // particle.r /= 2
+        particle.velocidad = randomEntre(5, 7);
+        //particle.color.dropOpacity(randomEntre(0.1, 0.9))
+        particles.push(particle);
+    }
+}
+function particleLive(p, i) {
+    //p.color.dropOpacity(randomEntre(0.01, 0.01))
+    p.color.dropOpacity(0.001);
+    //p.r *= 0.95
+    p.draw();
+    if (p.color.getOpacity() < 0) {
+        particles.splice(i, 1);
+    }
+    if (p.velocidad > 20) {
+        p.velocidad *= 0.5;
+    }
+    else {
+        p.velocidad++;
+    }
+}
 function shoot(x, y) {
     for (var i = 0; i < vivos.length; i++) {
         if (Math.sqrt(Math.pow(x - vivos[i].x, 2) + Math.pow(y - vivos[i].y, 2)) < vivos[i].r) {
-            muertos.push(vivos[i]);
+            // muertos.push(vivos[i])
+            particlesCreate(x, y, vivos[i].color);
             vivos.splice(i, 1);
             vivos.push(new Circle(context));
             break;
@@ -40,8 +89,9 @@ function shoot(x, y) {
 }
 function step(timestamp) {
     clearScreen();
-    muertos.forEach(shrink);
-    muertos = muertos.filter(isDying);
+    //    muertos.forEach(shrink)
+    //   muertos = muertos.filter(isDying)
+    particles.forEach(particleLive);
     vivos.forEach(live);
     window.requestAnimationFrame(step);
 }
@@ -59,8 +109,10 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-var muertos = new Array();
+//let muertos = new Array();
 var vivos = new Array();
+var particles = new Array();
+var particlesCount = 10;
 var ballSize = (canvas.width + canvas.height) / 30;
 var cantBolas = 3;
 var velocidad = 1;
