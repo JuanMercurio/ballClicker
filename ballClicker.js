d7bd08bb2dab27111be7ae1d632d6d5f1fef4113
Juan Mercurio <juanmercuriobadii@gmail.com>
2022-07-27 22:31:14 -0300

    Counter and 27 egg

--- ballClicker.js
+++ ballClicker.js
@@ -50,14 +50,13 @@ function randomEntre(min, max) {
     return Math.random() * (max - min) + min;
 }
 function particlesCreate(x, y, color) {
-    for (var i = 0; i < particlesCount; i++) {
+    for (var i = 0; i < Math.min(streak, 30); i++) {
         var particle = new Circle(context);
         particle.x = x;
         particle.y = y;
         particle.color.random();
-        particle.r /= 10;
-        particle.velocidad = randomEntre(5, 7);
-        //particle.color.dropOpacity(randomEntre(0.1, 0.9))
+        particle.r = 7;
+        particle.velocidad = randomEntre(2, 3);
         particles.push(particle);
     }
 }
@@ -70,49 +69,50 @@ function particleLive(p, i) {
     if (p.color.getOpacity() < 0) {
         particles.splice(i, 1);
     }
-    p.color.dropOpacity(0.01);
+    p.color.dropOpacity((1 / (p.r + 1)) * 0.01);
     p.r *= 0.96;
-    // if (p.velocidad > 6) {
-    //     p.color.dropOpacity(0.012)
-    //     p.r *= 0.96
-    //     p.velocidad *= 0.5
-    // } else {
-    //     p.r *= 0.9
-    //     p.color.dropOpacity(0.02)
-    //     p.velocidad += 2
-    // }
 }
-function specialsCreate(x, y) {
-    for (var i = 0; i < particlesCount; i++) {
-        var special = new Circle(context);
-        special.x = x;
-        special.y = y;
-        special.color.random();
-        special.velocidad = randomEntre(1, 4);
-        specials.push(special);
+function festejo(x, y) {
+    for (var i = 0; i < 50; i++) {
+        var particula = new Circle(context);
+        particula.color = new Color(255, 215, 0, 1);
+        particula.x = x;
+        particula.y = y;
+        particula.r = 5;
+        particula.velocidad = randomEntre(10, 15);
+        particles.push(particula);
     }
 }
 function shoot(x, y) {
     for (var i = 0; i < vivos.length; i++) {
         var root = Math.sqrt(Math.pow(x - vivos[i].x, 2) + Math.pow(y - vivos[i].y, 2));
-        if (root < vivos[i].r) {
-            particlesCreate(x, y, vivos[i].color);
+        if (root <= vivos[i].r) {
+            streak += 1;
+            time = 50;
+            console.log(streak);
+            if (streak == 27) {
+                festejo(vivos[i].x, vivos[i].y);
+            }
+            else {
+                particlesCreate(x, y, vivos[i].color);
+            }
             vivos.splice(i, 1);
             vivos.push(new Circle(context));
-            //(Math.floor(randomEntre(1,15)) == 1)  
-            if (root < vivos[i].r / 4) {
-                console.log('entre en el famoso');
-                specialsCreate(x, y);
-            }
-            break;
+            return;
         }
     }
+    // console.log("le pifiesta perro")
+    streak = 0;
 }
 function step(timestamp) {
     clearScreen();
     particles.forEach(particleLive);
-    specials.forEach(specialLive);
+    // specials.forEach(specialLive)
     vivos.forEach(live);
+    time -= 1;
+    if (time < 0) {
+        streak = 0;
+    }
     window.requestAnimationFrame(step);
 }
 function shrink(circle) {
@@ -120,10 +120,16 @@ function shrink(circle) {
     circle.r = Math.max(0, circle.r -= 2);
 }
 function live(circle) {
+    if (Math.floor(circle.x - circle.r) < 0 || Math.floor(circle.x + circle.r) > canvas.width) {
+        circle.direccion[0] = circle.direccion[0] * -1;
+    }
+    if (Math.floor(circle.y - circle.r) < 0 || Math.floor(circle.y + circle.r) > canvas.height) {
+        circle.direccion[1] = circle.direccion[1] * -1;
+    }
     circle.draw();
 }
 function isDying(circle) {
-    return circle.r != 0;
+    return circle.r > 0;
 }
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
@@ -133,13 +139,15 @@ var vivos = new Array();
 var specials = new Array();
 var particles = new Array();
 var particlesCount = 20;
+var streak = 0;
+var time = 30;
 var ballSize = (canvas.width + canvas.height) / 30;
 var cantBolas = 3;
-var velocidad = 1;
+var velocidad = 2;
 for (var i = 0; i < cantBolas; i++) {
     vivos.push(new Circle(context));
 }
-document.addEventListener("pointerdown", function (event) {
+document.addEventListener("mousedown", function (event) {
     shoot(event.clientX, event.clientY);
 });
 window.requestAnimationFrame(step);
